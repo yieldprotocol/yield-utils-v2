3c56b52cf5ccd82f11417caa3a0d6305cc1fd73b
Alberto Cuesta Cañada <mail@albertocuesta.es>
2021-04-19 09:27:09 +0100

    tweak: removed revertOnFail from multicall

--- contracts/Multicall.sol
+++ contracts/Multicall.sol
@@ -10,19 +10,18 @@ import "hardhat/console.sol";
 contract Multicall {
     /// @notice Allows batched call to self (this contract).
     /// @param calls An array of inputs for each call.
-    /// @param revertOnFail If True then reverts after a failed call and stops doing further calls.
     /// @return successes An array indicating the success of a call, mapped one-to-one to `calls`.
     /// @return results An array with the returned data of each function call, mapped one-to-one to `calls`.
     // F1: External is ok here because this is the batch function, adding it to a batch makes no sense
     // F2: Calls in the batch may be payable, delegatecall operates in the same context, so each call in the batch has access to msg.value
     // C3: The length of the loop is fully under user control, so can't be exploited
     // C7: Delegatecall is only used on the same contract, so it's safe
-    function multicall(bytes[] calldata calls, bool revertOnFail) external payable returns (bool[] memory successes, bytes[] memory results) {
+    function multicall(bytes[] calldata calls) external payable returns (bool[] memory successes, bytes[] memory results) {
         successes = new bool[](calls.length);
         results = new bytes[](calls.length);
         for (uint256 i = 0; i < calls.length; i++) {
             (bool success, bytes memory result) = address(this).delegatecall(calls[i]);
-            if (!success && revertOnFail) revert(RevertMsgExtractor.getRevertMsg(result));
+            if (!success) revert(RevertMsgExtractor.getRevertMsg(result));
             successes[i] = success;
             results[i] = result;
         }
