3b781e79eb5e057871d9891f400feef7153f8a81
Alberto Cuesta Cañada <mail@albertocuesta.es>
2021-04-19 10:57:52 +0100

    tweak: import yield-utils 1.3.0

--- contracts/mocks/ERC20Mock.sol
+++ contracts/mocks/ERC20Mock.sol
@@ -8,7 +8,7 @@ contract ERC20Mock is ERC20Permit  {
     constructor(
         string memory name,
         string memory symbol
-    ) ERC20Permit(name, symbol) { }
+    ) ERC20Permit(name, symbol, 18) { }
 
     /// @dev Give tokens to whoever asks for them.
     function mint(address to, uint256 amount) public virtual {
