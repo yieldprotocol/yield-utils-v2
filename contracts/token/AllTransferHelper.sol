15038e4e9729ff3f58f9ef8aa5a5ec9ffcdac98d
Alberto Cuesta Cañada <mail@albertocuesta.es>
2021-05-05 09:58:28 +0100

    tweak: transfer helper natspec

--- contracts/token/AllTransferHelper.sol
+++ contracts/token/AllTransferHelper.sol
@@ -8,7 +8,12 @@ import "../utils/RevertMsgExtractor.sol";
 
 
 // helper methods for interacting with ERC20 tokens and sending ETH that do not consistently return true/false
-library AllTransferHelper {
+library TransferHelper {
+    /// @notice Transfers tokens from msg.sender to a recipient
+    /// @dev Errors with the underlying revert message if transfer fails
+    /// @param token The contract address of the token which will be transferred
+    /// @param to The recipient of the transfer
+    /// @param value The value of the transfer
     function safeTransfer(
         IERC20 token,
         address to,
@@ -18,6 +23,12 @@ library AllTransferHelper {
         if (!(success && (data.length == 0 || abi.decode(data, (bool))))) revert(RevertMsgExtractor.getRevertMsg(data));
     }
 
+    /// @notice Transfers tokens from the targeted address to the given destination
+    /// @dev Errors with the underlying revert message if transfer fails
+    /// @param token The contract address of the token to be transferred
+    /// @param from The originating address from which the tokens will be transferred
+    /// @param to The destination address of the transfer
+    /// @param value The amount to be transferred
     function safeTransferFrom(
         IERC20 token,
         address from,
@@ -28,6 +39,10 @@ library AllTransferHelper {
         if (!(success && (data.length == 0 || abi.decode(data, (bool))))) revert(RevertMsgExtractor.getRevertMsg(data));
     }
 
+    /// @notice Transfers ETH to the recipient address
+    /// @dev Errors with the underlying revert message if transfer fails
+    /// @param to The destination of the transfer
+    /// @param value The value to be transferred
     function safeTransferETH(address payable to, uint256 value) internal {
         (bool success, bytes memory data) = to.call{value: value}(new bytes(0));
         if (!success) revert(RevertMsgExtractor.getRevertMsg(data));
--- contracts/token/TransferHelper.sol
+++ contracts/token/TransferHelper.sol
@@ -7,8 +7,13 @@ import "./IERC20.sol";
 import "../utils/RevertMsgExtractor.sol";
 
 
-// helper methods for interacting with ERC20 tokens and sending ETH that do not consistently return true/false
+// helper methods for transferring ERC20 tokens that do not consistently return true/false
 library TransferHelper {
+    /// @notice Transfers tokens from msg.sender to a recipient
+    /// @dev Errors with the underlying revert message if transfer fails
+    /// @param token The contract address of the token which will be transferred
+    /// @param to The recipient of the transfer
+    /// @param value The value of the transfer
     function safeTransfer(
         IERC20 token,
         address to,
@@ -16,20 +21,5 @@ library TransferHelper {
     ) internal {
         (bool success, bytes memory data) = address(token).call(abi.encodeWithSelector(IERC20.transfer.selector, to, value));
         if (!(success && (data.length == 0 || abi.decode(data, (bool))))) revert(RevertMsgExtractor.getRevertMsg(data));
-    }
-
-    function safeTransferFrom(
-        IERC20 token,
-        address from,
-        address to,
-        uint256 value
-    ) internal {
-        (bool success, bytes memory data) = address(token).call(abi.encodeWithSelector(IERC20.transferFrom.selector, from, to, value));
-        if (!(success && (data.length == 0 || abi.decode(data, (bool))))) revert(RevertMsgExtractor.getRevertMsg(data));
-    }
-
-    function safeTransferETH(address payable to, uint256 value) internal {
-        (bool success, bytes memory data) = to.call{value: value}(new bytes(0));
-        if (!success) revert(RevertMsgExtractor.getRevertMsg(data));
     }
 }
