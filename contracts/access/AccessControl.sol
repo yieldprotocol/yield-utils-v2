11c3d49193b77ba5ac7e653551e5958a9e2609ef
uivlis <vladsilviufarcas@gmail.com>
2021-06-22 12:43:21 +0300

    Use lockRole for LOCK admin

--- contracts/access/AccessControl.sol
+++ contracts/access/AccessControl.sol
@@ -127,6 +127,7 @@ contract AccessControl {
      * - the caller must have ``role``'s admin role.
      */
     function setRoleAdmin(bytes4 role, bytes4 adminRole) external virtual admin(role) {
+        require(role != LOCK, "Use `lockRole`");
         _setRoleAdmin(role, adminRole);
     }
 
