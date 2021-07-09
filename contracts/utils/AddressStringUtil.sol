7d9e1c6376ddf4620ffd316f15f67fbfc8038db0
Alberto Cuesta Cañada <mail@albertocuesta.es>
2021-07-09 16:36:14 +0100

    tweak: cheaper string manipulation

--- contracts/utils/AddressStringUtil.sol
+++ contracts/utils/AddressStringUtil.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.5.0;
 
 library AddressStringUtil {
     // converts an address to the uppercase hex string, extracting only len bytes (up to 20, multiple of 2)
-    function toAsciiString(address addr, uint256 len) public pure returns (string memory) {
+    function toAsciiString(address addr, uint256 len) internal pure returns (string memory) {
         require(len % 2 == 0 && len > 0 && len <= 40, "AddressStringUtil: INVALID_LEN");
         bytes memory s = new bytes(len);
         uint256 addrNum = uint256(uint160(addr));
