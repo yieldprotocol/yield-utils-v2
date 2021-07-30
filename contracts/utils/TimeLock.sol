2d205bcf367202bb4a3be353b87e2f69830021ef
Alberto Cuesta Cañada <mail@albertocuesta.es>
2021-07-30 12:06:12 +0100

    feat: unit-tested and cleaned up

--- contracts/utils/TimeLock.sol
+++ contracts/utils/TimeLock.sol
@@ -5,16 +5,22 @@ pragma solidity ^0.8.0;
 import "../access/AccessControl.sol";
 import "./RevertMsgExtractor.sol";
 
+interface ITimeLock {
+    function setDelay(uint32 delay_) external;
+    function queue(address[] memory targets, bytes[] memory data, uint32 eta) external returns (bytes32 txHash);
+    function cancel(address[] memory targets, bytes[] memory data, uint32 eta) external;
+    function execute(address[] memory targets, bytes[] memory data, uint32 eta) external returns (bytes[] memory results);
+}
 
-contract TimeLock is AccessControl {
+contract TimeLock is ITimeLock, AccessControl {
     uint32 public constant GRACE_PERIOD = 14 days;
     uint32 public constant MINIMUM_DELAY = 2 days;
     uint32 public constant MAXIMUM_DELAY = 30 days;
 
     event DelaySet(uint32 indexed delay);
-    event Cancelled(bytes32 indexed hash, address[] indexed targets, bytes[] data, uint32 eta);
-    event Executed(bytes32 indexed hash, address[] indexed targets, bytes[] data, uint32 eta);
-    event Queued(bytes32 indexed hash, address[] indexed targets, bytes[] data, uint32 eta);
+    event Cancelled(bytes32 indexed txHash, address[] indexed targets, bytes[] data, uint32 eta);
+    event Executed(bytes32 indexed txHash, address[] indexed targets, bytes[] data, uint32 eta);
+    event Queued(bytes32 indexed txHash, address[] indexed targets, bytes[] data, uint32 eta);
 
     uint32 public delay;
     mapping (bytes32 => bool) public queued;
@@ -23,12 +29,12 @@ contract TimeLock is AccessControl {
         delay = MINIMUM_DELAY;
 
         // msg.sender can queue, cancel, and execute transactions
-        _grantRole(bytes4(keccak256("queue(address[],bytes[],uint32)")), msg.sender);
-        _grantRole(bytes4(keccak256("cancel(address[],bytes[],uint32)")), msg.sender);
-        _grantRole(bytes4(keccak256("execute(address[],bytes[],uint32)")), msg.sender);
+        _grantRole(ITimeLock.queue.selector, msg.sender); // bytes4(keccak256("queue(address[],bytes[],uint32)"))
+        _grantRole(ITimeLock.cancel.selector, msg.sender); // bytes4(keccak256("cancel(address[],bytes[],uint32)"))
+        _grantRole(ITimeLock.execute.selector, msg.sender); // bytes4(keccak256("execute(address[],bytes[],uint32)"))
 
         // Changing the delay must now be executed through this TimeLock contract
-        _grantRole(bytes4(keccak256("setDelay(uint32)")), address(this));
+        _grantRole(ITimeLock.setDelay.selector, address(this)); // bytes4(keccak256("setDelay(uint32)"))
 
         // Granting roles (queue, cancel, execute, setDelay) must now be executed through this TimeLock contract
         _grantRole(ROOT, address(this));
@@ -36,7 +42,7 @@ contract TimeLock is AccessControl {
     }
 
     /// @dev Change the delay for queueing and executing transactions
-    function setDelay(uint32 delay_) external auth {
+    function setDelay(uint32 delay_) external override auth {
         require(delay_ >= MINIMUM_DELAY, "Must exceed minimum delay.");
         require(delay_ <= MAXIMUM_DELAY, "Must not exceed maximum delay.");
         delay = delay_;
@@ -46,43 +52,43 @@ contract TimeLock is AccessControl {
 
     /// @dev Schedule a transaction batch for execution between `eta` and `eta + GRACE_PERIOD`
     function queue(address[] memory targets, bytes[] memory data, uint32 eta)
-        external auth returns (bytes32 hash)
+        external override auth returns (bytes32 txHash)
     {
         require(targets.length == data.length, "Mismatched inputs");
         require(eta >= uint32(block.timestamp) + delay, "Must satisfy delay.");
         
-        hash = keccak256(abi.encode(targets, data, eta));
-        queued[hash] = true;
-        emit Queued(hash, targets, data, eta);
+        txHash = keccak256(abi.encode(targets, data, eta));
+        queued[txHash] = true;
+        emit Queued(txHash, targets, data, eta);
     }
 
     /// @dev Cancel a scheduled  transaction batch
     function cancel(address[] memory targets, bytes[] memory data, uint32 eta)
-        external auth
+        external override auth
     {
         require(targets.length == data.length, "Mismatched inputs");
-        bytes32 hash = keccak256(abi.encode(targets, data, eta));
-        queued[hash] = false;
-        emit Cancelled(hash, targets, data, eta);
+        bytes32 txHash = keccak256(abi.encode(targets, data, eta));
+        queued[txHash] = false;
+        emit Cancelled(txHash, targets, data, eta);
     }
 
     /// @dev Execute a transaction batch
     function execute(address[] memory targets, bytes[] memory data, uint32 eta)
-        external auth returns (bytes[] memory results)
+        external override auth returns (bytes[] memory results)
     {
         require(targets.length == data.length, "Mismatched inputs");
-        require(uint32(block.timestamp) >= eta, "Time lock not reached.");
+        require(uint32(block.timestamp) >= eta, "ETA not reached.");
         require(uint32(block.timestamp) <= eta + GRACE_PERIOD, "Transaction is stale.");
-        bytes32 hash = keccak256(abi.encode(targets, data, eta));
-        require(queued[hash] == true, "Transaction hasn't been queued.");
-        queued[hash] = false;
+        bytes32 txHash = keccak256(abi.encode(targets, data, eta));
+        require(queued[txHash] == true, "Transaction hasn't been queued.");
+        queued[txHash] = false;
 
         results = new bytes[](targets.length);
         for (uint256 i = 0; i < targets.length; i++){
             (bool success, bytes memory result) = targets[i].call(data[i]);
             if (!success) revert(RevertMsgExtractor.getRevertMsg(result));
             results[i] = result;
-            emit Executed(hash, targets, data, eta);
+            emit Executed(txHash, targets, data, eta);
         }
     }
 }
---
+++ test/005_timelock.ts
@@ -0,0 +1,165 @@
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address'
+
+import { id } from "../src/index";
+
+import ERC20MockArtifact from '../artifacts/contracts/mocks/ERC20Mock.sol/ERC20Mock.json'
+import TimeLockArtifact from '../artifacts/contracts/utils/TimeLock.sol/TimeLock.json'
+import { ERC20Mock as ERC20 } from '../typechain/ERC20Mock'
+import { TimeLock } from '../typechain/TimeLock'
+
+import { BigNumber } from 'ethers'
+
+import { ethers, waffle } from 'hardhat'
+import { expect } from 'chai'
+const { deployContract, loadFixture } = waffle
+
+describe('TimeLock', async function () {
+  this.timeout(0)
+
+  let ownerAcc: SignerWithAddress
+  let owner: string
+  let user1: string
+  let user1Acc: SignerWithAddress
+  let user2: string
+  let user2Acc: SignerWithAddress
+
+  let target1: ERC20
+  let target2: ERC20
+  let timelock: TimeLock
+
+  let timestamp: number
+  let resetChain: number
+
+  before(async () => {
+    resetChain = await ethers.provider.send('evm_snapshot', [])
+    const signers = await ethers.getSigners()
+    ownerAcc = signers[0]
+    owner = ownerAcc.address
+    user1Acc = signers[1]
+    user1 = user1Acc.address
+    user2Acc = signers[2]
+    user2 = user2Acc.address
+  })
+
+  after(async () => {
+    await ethers.provider.send('evm_revert', [resetChain])
+  })
+
+  beforeEach(async () => {
+    target1 = (await deployContract(ownerAcc, ERC20MockArtifact, ['Target1', 'TG1'])) as ERC20
+    target2 = (await deployContract(ownerAcc, ERC20MockArtifact, ['Target2', 'TG2'])) as ERC20
+    timelock = (await deployContract(ownerAcc, TimeLockArtifact, [])) as TimeLock
+    ;({ timestamp } = await ethers.provider.getBlock('latest'))
+  })
+
+  it('doesn\'t allow governance changes to owner', async () => {
+    await expect(timelock.setDelay(0)).to.be.revertedWith('Access denied')
+    await expect(timelock.grantRole('0x00000000', owner)).to.be.revertedWith('Only admin')
+    await expect(timelock.grantRole(id('queue(address[],bytes[],uint32)'), user1)).to.be.revertedWith('Only admin')
+    await expect(timelock.revokeRole(id('queue(address[],bytes[],uint32)'), owner)).to.be.revertedWith('Only admin')
+  })
+
+  it('doesn\'t allow mismatched inputs', async () => {
+    const targets = [target1.address, target2.address]
+    const data = [target1.interface.encodeFunctionData('mint', [owner, 1])]
+    const eta = timestamp + await timelock.delay()
+    await expect(timelock.queue(targets, data, eta)).to.be.revertedWith('Mismatched inputs')
+    await expect(timelock.cancel(targets, data, eta)).to.be.revertedWith('Mismatched inputs')
+    await expect(timelock.execute(targets, data, eta)).to.be.revertedWith('Mismatched inputs')
+  })
+
+  it('doesn\'t allow to queue for execution before `delay()`', async () => {
+    const targets = [target1.address]
+    const data = [target1.interface.encodeFunctionData('mint', [owner, 1])]
+    const eta = timestamp
+    await expect(timelock.queue(targets, data, eta)).to.be.revertedWith('Must satisfy delay')
+  })
+
+  it('doesn\'t allow to execute before eta', async () => {
+    const targets = [target1.address]
+    const data = [target1.interface.encodeFunctionData('mint', [owner, 1])]
+    const eta = timestamp + 100
+    await expect(timelock.execute(targets, data, eta)).to.be.revertedWith('ETA not reached')
+  })
+
+  it('doesn\'t allow to execute after grace period', async () => {
+    const targets = [target1.address]
+    const data = [target1.interface.encodeFunctionData('mint', [owner, 1])]
+    const eta = timestamp - (await timelock.GRACE_PERIOD())
+    await expect(timelock.execute(targets, data, eta)).to.be.revertedWith('Transaction is stale')
+  })
+
+  it('doesn\'t allow to execute if not queued', async () => {
+    const targets = [target1.address]
+    const data = [target1.interface.encodeFunctionData('mint', [owner, 1])]
+    const eta = timestamp
+    await expect(timelock.execute(targets, data, eta)).to.be.revertedWith('Transaction hasn\'t been queued.')
+  })
+
+  it('queues a transaction', async () => {
+    const targets = [target1.address]
+    const data = [target1.interface.encodeFunctionData('mint', [owner, 1])]
+    const eta = timestamp + await timelock.delay() + 100
+    const txHash = await timelock.callStatic.queue(targets, data, eta)
+
+    await expect(await timelock.queue(targets, data, eta))
+      .to.emit(timelock, 'Queued')
+//      .withArgs(txHash, targets, data, eta)
+    expect(await timelock.queued(txHash)).to.be.true
+  })
+
+  describe('with a queued transaction', async () => {
+    let snapshotId: string
+    let timestamp: number
+    let targets: string[]
+    let data: string[]
+    let eta: number
+    let txHash: string
+
+    beforeEach(async () => {
+      ;({ timestamp } = await ethers.provider.getBlock('latest'))
+      targets = [
+        target1.address,
+        target2.address,
+      ]
+      data = [
+        target1.interface.encodeFunctionData('mint', [owner, 1]),
+        target2.interface.encodeFunctionData('approve', [owner, 1]),
+      ]
+      eta = timestamp + await timelock.delay() + 100
+      txHash = await timelock.callStatic.queue(targets, data, eta)
+      await timelock.queue(targets, data, eta)
+    })
+
+    it('cancels a transaction', async () => {
+      await expect(await timelock.cancel(targets, data, eta))
+        .to.emit(timelock, 'Cancelled')
+//        .withArgs(txHash, targets, data, eta)
+      expect(await timelock.queued(txHash)).to.be.false
+    })
+
+    describe('once the eta arrives', async () => {
+      beforeEach(async () => {
+        snapshotId = await ethers.provider.send('evm_snapshot', [])
+        await ethers.provider.send('evm_mine', [eta])
+      })
+
+      afterEach(async () => {
+        await ethers.provider.send('evm_revert', [snapshotId])
+      })
+
+      it('executes a transaction', async () => {
+        await expect(await timelock.execute(targets, data, eta))
+          .to.emit(timelock, 'Executed')
+//          .withArgs(txHash, targets, data, eta)
+          .to.emit(target1, 'Transfer')
+//          .withArgs(null, owner, 1)
+          .to.emit(target2, 'Approval')
+//          .withArgs(owner, owner, 1)
+        expect(await timelock.queued(txHash)).to.be.false
+        expect(await target1.balanceOf(owner)).to.equal(1)
+        expect(await target2.allowance(timelock.address, owner)).to.equal(1)
+      })
+    })
+  })
+})
