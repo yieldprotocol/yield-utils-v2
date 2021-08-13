2ddae2927144a0a6a184987777ab3b268d0f5172
Alberto Cuesta Cañada <mail@albertocuesta.es>
2021-08-13 18:41:38 +0100

    feat: more casts and TransferHelper

--- contracts/token/TransferHelper.sol
+++ contracts/token/TransferHelper.sol
@@ -7,7 +7,7 @@ import "./IERC20.sol";
 import "../utils/RevertMsgExtractor.sol";
 
 
-// helper methods for transferring ERC20 tokens that do not consistently return true/false
+// helper methods for interacting with ERC20 tokens and sending ETH that do not consistently return true/false
 library TransferHelper {
     /// @notice Transfers tokens from msg.sender to a recipient
     /// @dev Errors with the underlying revert message if transfer fails
@@ -21,5 +21,30 @@ library TransferHelper {
     ) internal {
         (bool success, bytes memory data) = address(token).call(abi.encodeWithSelector(IERC20.transfer.selector, to, value));
         if (!(success && (data.length == 0 || abi.decode(data, (bool))))) revert(RevertMsgExtractor.getRevertMsg(data));
+    }
+
+    /// @notice Transfers tokens from the targeted address to the given destination
+    /// @dev Errors with the underlying revert message if transfer fails
+    /// @param token The contract address of the token to be transferred
+    /// @param from The originating address from which the tokens will be transferred
+    /// @param to The destination address of the transfer
+    /// @param value The amount to be transferred
+    function safeTransferFrom(
+        IERC20 token,
+        address from,
+        address to,
+        uint256 value
+    ) internal {
+        (bool success, bytes memory data) = address(token).call(abi.encodeWithSelector(IERC20.transferFrom.selector, from, to, value));
+        if (!(success && (data.length == 0 || abi.decode(data, (bool))))) revert(RevertMsgExtractor.getRevertMsg(data));
+    }
+
+    /// @notice Transfers ETH to the recipient address
+    /// @dev Errors with the underlying revert message if transfer fails
+    /// @param to The destination of the transfer
+    /// @param value The value to be transferred
+    function safeTransferETH(address payable to, uint256 value) internal {
+        (bool success, bytes memory data) = to.call{value: value}(new bytes(0));
+        if (!success) revert(RevertMsgExtractor.getRevertMsg(data));
     }
 }
