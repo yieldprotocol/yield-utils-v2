6b2ae46555521e8e096b043f781eb48779a79a6f
Alberto Cuesta Cañada <mail@albertocuesta.es>
2021-08-20 07:49:55 +0100

    test: Timelock.sol

--- contracts/utils/Timelock.sol
+++ contracts/utils/Timelock.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-// Inspired on TimeLock.sol from Compound.
+// Inspired on Timelock.sol from Compound.
 // Special thanks to BoringSolidity for his feedback.
 
 pragma solidity ^0.8.0;
@@ -7,11 +7,12 @@ import "../access/AccessControl.sol";
 import "./RevertMsgExtractor.sol";
 import "./IsContract.sol";
 
+
 interface ITimelock {
     function setDelay(uint32 delay_) external;
-    function schedule(address[] calldata targets, bytes[] calldata data) external returns (bytes32 txHash);
-    function scheduleRepeated(address[] calldata targets, bytes[] calldata data, uint256 salt) external returns (bytes32 txHash);
-    function approve(bytes32 txHash) external;
+    function propose(address[] calldata targets, bytes[] calldata data) external returns (bytes32 txHash);
+    function proposeRepeated(address[] calldata targets, bytes[] calldata data, uint256 salt) external returns (bytes32 txHash);
+    function approve(bytes32 txHash) external returns (uint32);
     function execute(address[] calldata targets, bytes[] calldata data) external returns (bytes[] calldata results);
     function executeRepeated(address[] calldata targets, bytes[] calldata data, uint256 salt) external returns (bytes[] calldata results);
 }
@@ -19,9 +20,9 @@ interface ITimelock {
 contract Timelock is ITimelock, AccessControl {
     using IsContract for address;
 
-    enum STATE { UNSCHEDULED, SCHEDULED, APPROVED }
+    enum STATE { UNKNOWN, PROPOSED, APPROVED }
 
-    struct Transaction {
+    struct Proposal {
         STATE state;
         uint32 eta;
     }
@@ -31,31 +32,31 @@ contract Timelock is ITimelock, AccessControl {
     uint32 public constant MAXIMUM_DELAY = 30 days;
 
     event DelaySet(uint256 indexed delay);
-    event Scheduled(bytes32 indexed txHash, address[] targets, bytes[] data);
+    event Proposed(bytes32 indexed txHash, address[] targets, bytes[] data);
     event Approved(bytes32 indexed txHash, uint32 eta);
     event Executed(bytes32 indexed txHash, address[] targets, bytes[] data);
 
     uint32 public delay;
-    mapping (bytes32 => Transaction) public transactions;
+    mapping (bytes32 => Proposal) public proposals;
 
     constructor(address governor) AccessControl() {
         delay = 0; // delay is set to zero initially to allow testing and configuration. Set to a different value to go live.
 
-        _grantRole(ITimelock.schedule.selector, governor); // On going live, it is recommended the schedule permission is granted to chosen individuals
-        _grantRole(ITimelock.scheduleRepeated.selector, governor); // On going live, it is recommended the schedule permission is granted to chosen individuals
-        _grantRole(ITimelock.approve.selector, governor); // On going live, it is recommended the approve permission is kept for the sole use of the governor
-        _grantRole(ITimelock.execute.selector, governor); // On going live, it is recommended the schedule permission is granted to chosen individuals
-        _grantRole(ITimelock.executeRepeated.selector, governor); // On going live, it is recommended the schedule permission is granted to chosen individuals
+        _grantRole(ITimelock.propose.selector, governor);           // On going live, it is recommended the propose permission is granted to chosen individuals
+        _grantRole(ITimelock.proposeRepeated.selector, governor);   // On going live, it is recommended the propose permission is granted to chosen individuals
+        _grantRole(ITimelock.approve.selector, governor);           // On going live, it is recommended the approve permission is kept for the sole use of the governor
+        _grantRole(ITimelock.execute.selector, governor);           // On going live, it is recommended the propose permission is granted to chosen individuals
+        _grantRole(ITimelock.executeRepeated.selector, governor);   // On going live, it is recommended the propose permission is granted to chosen individuals
 
-        // Changing the delay must now be executed through this TimeLock contract
+        // Changing the delay must now be executed through this Timelock contract
         _grantRole(ITimelock.setDelay.selector, address(this)); // bytes4(keccak256("setDelay(uint256)"))
 
-        // Granting roles (schedule, cancel, execute, setDelay) must now be executed through this TimeLock contract
+        // Granting roles (propose, cancel, execute, setDelay) must now be executed through this Timelock contract
         _grantRole(ROOT, address(this));
         _revokeRole(ROOT, msg.sender);
     }
 
-    /// @dev Change the delay for queueing and executing transactions
+    /// @dev Change the delay for queueing and executing proposals
     function setDelay(uint32 delay_) external override auth {
         require(delay_ >= MINIMUM_DELAY, "Must exceed minimum delay.");
         require(delay_ <= MAXIMUM_DELAY, "Must not exceed maximum delay.");
@@ -64,42 +65,43 @@ contract Timelock is ITimelock, AccessControl {
         emit DelaySet(delay_);
     }
 
-    /// @dev Schedule a transaction batch for execution between `eta` and `eta + GRACE_PERIOD`
-    function schedule(address[] calldata targets, bytes[] calldata data)
+    /// @dev Propose a transaction batch for execution between `eta` and `eta + GRACE_PERIOD`
+    function propose(address[] calldata targets, bytes[] calldata data)
         external override auth returns (bytes32 txHash)
     {
-        return _schedule(targets, data, 0);
+        return _propose(targets, data, 0);
     }
 
-    /// @dev Schedule a transaction batch for execution between `eta` and `eta + GRACE_PERIOD`
-    /// @param salt Unique identifier for the transaction when repeatedly scheduled. Chosen by scheduler.
-    function scheduleRepeated(address[] calldata targets, bytes[] calldata data, uint256 salt)
+    /// @dev Propose a transaction batch for execution between `eta` and `eta + GRACE_PERIOD`
+    /// @param salt Unique identifier for the transaction when repeatedly proposed. Chosen by governor.
+    function proposeRepeated(address[] calldata targets, bytes[] calldata data, uint256 salt)
         external override auth returns (bytes32 txHash)
     {
-        return _schedule(targets, data, salt);
+        return _propose(targets, data, salt);
     }
 
-    /// @dev Schedule a transaction batch for execution between `eta` and `eta + GRACE_PERIOD`
-    function _schedule(address[] calldata targets, bytes[] calldata data, uint256 salt)
+    /// @dev Propose a transaction batch for execution between `eta` and `eta + GRACE_PERIOD`
+    function _propose(address[] calldata targets, bytes[] calldata data, uint256 salt)
         private auth returns (bytes32 txHash)
     {
         require(targets.length == data.length, "Mismatched inputs");
         txHash = keccak256(abi.encode(targets, data, salt));
-        require(transactions[txHash].state == STATE.UNSCHEDULED, "Transaction already scheduled.");
-        transactions[txHash].state = STATE.SCHEDULED;
-        emit Scheduled(txHash, targets, data);
+        require(proposals[txHash].state == STATE.UNKNOWN, "Already proposed.");
+        proposals[txHash].state = STATE.PROPOSED;
+        emit Proposed(txHash, targets, data);
     }
 
-    /// @dev Cancel a scheduled transaction batch
+    /// @dev Cancel a proposed transaction batch
     function approve(bytes32 txHash)
-        external override auth
+        external override auth returns (uint32 eta)
     {
-        Transaction memory transaction = transactions[txHash];
-        require(transaction.state == STATE.SCHEDULED, "Transaction not scheduled.");
-        transaction.state = STATE.APPROVED;
-        transaction.eta == uint32(block.timestamp) + delay;
-        transactions[txHash] = transaction;
-        emit Approved(txHash, transaction.eta);
+        Proposal memory proposal = proposals[txHash];
+        require(proposal.state == STATE.PROPOSED, "Not proposed.");
+        eta = uint32(block.timestamp) + delay;
+        proposal.state = STATE.APPROVED;
+        proposal.eta = eta;
+        proposals[txHash] = proposal;
+        emit Approved(txHash, eta);
     }
 
     /// @dev Execute a transaction batch
@@ -110,7 +112,7 @@ contract Timelock is ITimelock, AccessControl {
     }
     
     /// @dev Execute a transaction batch
-    /// @param salt Unique identifier for the transaction when repeatedly scheduled. Chosen by scheduler.
+    /// @param salt Unique identifier for the transaction when repeatedly proposed. Chosen by governor.
     function executeRepeated(address[] calldata targets, bytes[] calldata data, uint256 salt)
         external override auth returns (bytes[] memory results)
     {
@@ -122,13 +124,13 @@ contract Timelock is ITimelock, AccessControl {
         private auth returns (bytes[] memory results)
     {
         bytes32 txHash = keccak256(abi.encode(targets, data, salt));
-        Transaction memory transaction = transactions[txHash];
-        require(transaction.state == STATE.APPROVED, "Transaction not approved.");
+        Proposal memory proposal = proposals[txHash];
 
-        require(uint32(block.timestamp) >= transaction.eta, "ETA not reached.");
-        require(uint32(block.timestamp) <= transaction.eta + GRACE_PERIOD, "Transaction is stale.");
+        require(proposal.state == STATE.APPROVED, "Not approved.");
+        require(uint32(block.timestamp) >= proposal.eta, "ETA not reached.");
+        require(uint32(block.timestamp) <= proposal.eta + GRACE_PERIOD, "Proposal is stale.");
 
-        delete transactions[txHash];
+        delete proposals[txHash];
 
         results = new bytes[](targets.length);
         for (uint256 i = 0; i < targets.length; i++){
