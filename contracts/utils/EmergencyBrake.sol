1552fcb92b1118e03d7522b5e019f94f687be163
#Mae <mae@hashedmae.gay>
2022-09-09 20:04:08 -0700

    changes and testing done

--- contracts/utils/EmergencyBrake.sol
+++ contracts/utils/EmergencyBrake.sol
@@ -6,15 +6,17 @@ import "../access/AccessControl.sol";
 
 interface IEmergencyBrake {
     struct Permission {
-        address contact;
+        address contact; /// contract for which a user holds auth priviliges
         bytes4[] signatures;
     }
 
-    function plan(address target, Permission[] calldata permissions) external returns (bytes32 txHash);
-    function cancel(bytes32 txHash) external;
-    function execute(bytes32 txHash) external;
-    function restore(bytes32 txHash) external;
-    function terminate(bytes32 txHash) external;
+    function plan(address target, Permission[] calldata permissions) external;
+    function addToPlan(address target, Permission calldata permission) external;
+    function removeFromPlan(address target, Permission calldata permission) external;
+    function cancel(address target) external;
+    function execute(address target) external;
+    function restore(address target) external;
+    function terminate(address target) external;
 }
 
 /// @dev EmergencyBrake allows to plan for and execute transactions that remove access permissions for a target
@@ -31,79 +33,135 @@ contract EmergencyBrake is AccessControl, IEmergencyBrake {
     struct Plan {
         State state;
         address target;
-        bytes permissions;
+        Permission[] permissions;
     }
 
-    event Planned(bytes32 indexed txHash, address indexed target);
-    event Cancelled(bytes32 indexed txHash);
-    event Executed(bytes32 indexed txHash, address indexed target);
-    event Restored(bytes32 indexed txHash, address indexed target);
-    event Terminated(bytes32 indexed txHash);
+    event Planned(address indexed target, Permission[] permissions);
+    event AddedTo(address indexed target, Permission toAdd);
+    event RemovedFrom(address indexed target, Permission toRemove);
+    event Cancelled(address indexed target);
+    event Executed(address indexed target);
+    event Restored(address indexed target);
+    event Terminated(address indexed target);
 
-    mapping (bytes32 => Plan) public plans;
+    mapping (address => Plan) public plans;
+    mapping (bytes => uint256) public permissionIndex;
 
     constructor(address planner, address executor) AccessControl() {
         _grantRole(IEmergencyBrake.plan.selector, planner);
+        _grantRole(IEmergencyBrake.addToPlan.selector, planner);
+        _grantRole(IEmergencyBrake.removeFromPlan.selector, planner);
         _grantRole(IEmergencyBrake.cancel.selector, planner);
         _grantRole(IEmergencyBrake.execute.selector, executor);
         _grantRole(IEmergencyBrake.restore.selector, planner);
         _grantRole(IEmergencyBrake.terminate.selector, planner);
-
-        // Granting roles (plan, cancel, execute, restore, terminate) is reserved to ROOT
-    }
-
-    /// @dev Compute the hash of a plan
-    function hash(address target, Permission[] calldata permissions)
-        external pure
-        returns (bytes32 txHash)
-    {
-        txHash = keccak256(abi.encode(target, permissions));
+        // Granting roles (plan, cancel, execute, restore, terminate, modifyPlan) is reserved to ROOT
     }
 
     /// @dev Register an access removal transaction
+    /// @param target address with auth privileges on contracts
     function plan(address target, Permission[] calldata permissions)
         external override auth
-        returns (bytes32 txHash)
     {
-        txHash = keccak256(abi.encode(target, permissions));
-        require(plans[txHash].state == State.UNPLANNED, "Emergency already planned for.");
+        require(plans[target].state == State.UNPLANNED, "Emergency already planned for.");
 
         // Removing or granting ROOT permissions is out of bounds for EmergencyBrake
         for (uint256 i = 0; i < permissions.length; i++){
-            for (uint256 j = 0; j < permissions[i].signatures.length; j++){
+            Permission memory _permission = permissions[i];
+            for (uint256 j = 0; j < permissions[i].signatures.length;){
                 require(
                     permissions[i].signatures[j] != ROOT,
                     "Can't remove ROOT"
                 );
+                unchecked{++j;}
             }
+            plans[target].permissions.push(permissions[i]);
+            bytes memory toIndex = abi.encode(_permission);
+            unchecked{++i;}
+            permissionIndex[toIndex] = i;
         }
 
-        plans[txHash] = Plan({
-            state: State.PLANNED,
-            target: target,
-            permissions: abi.encode(permissions)
-        });
-        emit Planned(txHash, target);
+        plans[target].state = State.PLANNED;
+        plans[target].target = target;
+        emit Planned(target, permissions);
     }
 
-    /// @dev Erase a planned access removal transaction
-    function cancel(bytes32 txHash)
+    /// @dev add a permission set to remove for a contact to an existing plan
+    /// @dev a contact can be added multiple times to a plan but ensures that all signatures are unique to prevent revert on execution
+    /// @param target address with auth privileges on a contract and a plan exists for
+    /// @param toAdd permission set that is being added to an existing plan
+    function addToPlan(address target, Permission calldata toAdd)
+        external override auth 
+    {   
+        Permission[] memory _permissions = plans[target].permissions;
+        require(plans[target].state == State.PLANNED, "Target not planned for");
+
+        
+        
+        for(uint i; i < _permissions.length;) {
+            if(_permissions[i].contact == toAdd.contact){
+                for(uint j; j < _permissions[i].signatures.length;){
+                    for(uint k; k < toAdd.signatures.length;){
+                        require(toAdd.signatures[k] != ROOT, "Can't remove ROOT");
+                        require(_permissions[i].signatures[j] != toAdd.signatures[k], "Signature already in plan");
+                        unchecked{++k;}
+                    }
+                    unchecked{++j;}
+                }
+            }    
+            unchecked{++i;}
+        }
+         
+        bytes memory toIndex = abi.encode(toAdd);
+        uint256 planId = _permissions.length;
+        require(permissionIndex[toIndex] == 0, "Permission set already in plan");
+        plans[target].permissions.push(toAdd);
+        permissionIndex[toIndex] = planId;
+        
+        emit AddedTo(target, toAdd);
+    }
+
+    /// @dev remove a permission set from an existing plan
+    /// @dev retains the order of permissions and updates their index
+    /// @param target address wuth auth privileges on a contract and a plan exists for
+    function removeFromPlan(address target, Permission calldata toRemove) 
         external override auth
     {
-        require(plans[txHash].state == State.PLANNED, "Emergency not planned for.");
-        delete plans[txHash];
-        emit Cancelled(txHash);
+        require(plans[target].state == State.PLANNED, "Target not planned for");
+        bytes memory toUnindex = abi.encode(toRemove);
+        require(permissionIndex[toUnindex] != 0, "Permission set not planned for");
+        uint256 planId = permissionIndex[toUnindex] - 1;
+        for(uint i = planId; i < plans[target].permissions.length - 1;)
+        {
+            plans[target].permissions[i] = plans[target].permissions[i + 1];
+            bytes memory toIndex = abi.encode(plans[target].permissions[i]);
+            unchecked{++i;}
+            permissionIndex[toIndex] = i;
+        }
+        plans[target].permissions.pop();
+        permissionIndex[toUnindex] = 0;
+        emit RemovedFrom(target, toRemove);
+    }
+
+
+    /// @dev Erase a planned access removal transaction
+    function cancel(address target)
+        external override auth
+    {
+        require(plans[target].state == State.PLANNED, "Emergency not planned for.");
+        delete plans[target];
+        emit Cancelled(target);
     }
 
     /// @dev Execute an access removal transaction
-    function execute(bytes32 txHash)
+    function execute(address target)
         external override auth
     {
-        Plan memory plan_ = plans[txHash];
+        Plan memory plan_ = plans[target];
         require(plan_.state == State.PLANNED, "Emergency not planned for.");
-        plans[txHash].state = State.EXECUTED;
+        plans[target].state = State.EXECUTED;
 
-        Permission[] memory permissions_ = abi.decode(plan_.permissions, (Permission[]));
+        Permission[] memory permissions_ = plan_.permissions;
 
         for (uint256 i = 0; i < permissions_.length; i++){
             // AccessControl.sol doesn't revert if revoking permissions that haven't been granted
@@ -119,18 +177,18 @@ contract EmergencyBrake is AccessControl, IEmergencyBrake {
                 contact.revokeRole(signature_, plan_.target);
             }
         }
-        emit Executed(txHash, plan_.target);
+        emit Executed(target);
     }
 
     /// @dev Restore the orchestration from an isolated target
-    function restore(bytes32 txHash)
+    function restore(address target)
         external override auth
     {
-        Plan memory plan_ = plans[txHash];
+        Plan memory plan_ = plans[target];
         require(plan_.state == State.EXECUTED, "Emergency plan not executed.");
-        plans[txHash].state = State.PLANNED;
+        plans[target].state = State.PLANNED;
 
-        Permission[] memory permissions_ = abi.decode(plan_.permissions, (Permission[]));
+        Permission[] memory permissions_ = plan_.permissions;
 
         for (uint256 i = 0; i < permissions_.length; i++){
             Permission memory permission_ = permissions_[i]; 
@@ -140,15 +198,15 @@ contract EmergencyBrake is AccessControl, IEmergencyBrake {
                 contact.grantRole(signature_, plan_.target);
             }
         }
-        emit Restored(txHash, plan_.target);
+        emit Restored(target);
     }
 
     /// @dev Remove the restoring option from an isolated target
-    function terminate(bytes32 txHash)
+    function terminate(address target)
         external override auth
     {
-        require(plans[txHash].state == State.EXECUTED, "Emergency plan not executed.");
-        delete plans[txHash];
-        emit Terminated(txHash);
+        require(plans[target].state == State.EXECUTED, "Emergency plan not executed.");
+        delete plans[target];
+        emit Terminated(target);
     }
 }
--- test/foundry/EBrake.t.sol
+++ test/foundry/EBrake.t.sol
@@ -4,20 +4,23 @@ pragma solidity ^0.8.0;
 import "forge-std/Test.sol";
 import "forge-std/console2.sol";
 
-import "../../contracts/utils/EmergencyBrakeV2a.sol";
+import "../../contracts/utils/EmergencyBrake.sol";
 import "../../contracts/mocks/RestrictedERC20Mock.sol";
+import "../../contracts/utils/Timelock.sol";
 
 abstract contract ZeroState is Test {
     enum State {UNPLANNED, PLANNED, EXECUTED}
-    EmergencyBrakeV2 public ebrake;
+    EmergencyBrake public ebrake;
     RestrictedERC20Mock public rToken;
+    Timelock public lock;
     address public deployer;
     address public planner;
     address public executor;
     address public tokenAdmin;
 
-    event Planned(address indexed target);
-    event Modified(address indexed target);
+    event Planned(address indexed target, IEmergencyBrake.Permission[] permissions);
+    event AddedTo(address indexed target, IEmergencyBrake.Permission toAdd);
+    event RemovedFrom(address indexed target, IEmergencyBrake.Permission toRemove);
     event Cancelled(address indexed target);
     event Executed(address indexed target);
     event Restored(address indexed target);
@@ -27,6 +30,7 @@ abstract contract ZeroState is Test {
 
     bytes4[] public signatures;
     IEmergencyBrake.Permission[] public permissions;
+    IEmergencyBrake.Permission public permission;
 
     function setUp() public virtual {
         vm.startPrank(deployer);
@@ -43,11 +47,14 @@ abstract contract ZeroState is Test {
         tokenAdmin = address(4);
         vm.label(tokenAdmin, "tokenAdmin");
 
-        ebrake = new EmergencyBrakeV2(planner, executor);
+        ebrake = new EmergencyBrake(planner, executor);
         vm.label(address(ebrake), "Emergency Brake contract");
 
         rToken = new RestrictedERC20Mock("FakeToken", "FT");
         vm.label(address(rToken), "Restricted Token contract");
+
+        lock = new Timelock(tokenAdmin, executor);
+        vm.label(address(lock), "Authed TimeLock contract");
 
         rToken.grantRole(RestrictedERC20Mock.mint.selector, tokenAdmin);
         rToken.grantRole(RestrictedERC20Mock.burn.selector, tokenAdmin);
@@ -58,32 +65,6 @@ abstract contract ZeroState is Test {
 }
 
 contract ZeroStateTest is ZeroState {
-     
-    function testRoles(uint256 amount) public {
-        uint256 preBalance = rToken.balanceOf(deployer);
-
-        console2.log("Ensure that roles are set properly and functioning");
-        console2.log("Minter can mint");
-        
-        vm.prank(tokenAdmin);
-        rToken.mint(deployer, amount);
-        assertEq(rToken.balanceOf(deployer), preBalance + amount);
-
-        console2.log("Burner can burn");
-        vm.prank(tokenAdmin);
-        rToken.burn(deployer, amount);
-        assertEq(rToken.balanceOf(deployer), preBalance);
-    }
-
-    function testCannotWithoutRole() public {
-        vm.startPrank(planner);
-        vm.expectRevert("Access denied");
-        rToken.mint(deployer, 10000000);
-        vm.expectRevert("Access denied");
-        rToken.burn(deployer, 10000000);
-        vm.stopPrank();
-    }
-
 
     function testPlan() public {
         bytes4 minterRole = RestrictedERC20Mock.mint.selector;
@@ -97,30 +78,28 @@ contract ZeroStateTest is ZeroState {
         permissions.push(permission_);
 
         vm.expectEmit(true, false, false, true);
-        emit Planned(tokenAdmin);
+        emit Planned(tokenAdmin, permissions);
         vm.prank(planner);
         ebrake.plan(tokenAdmin, permissions);
 
-        (EmergencyBrakeV2.State state_,
-            ,
+        (EmergencyBrake.State state_,
+            
         ) = ebrake.plans(tokenAdmin);
 
-       bool isPlanned = EmergencyBrakeV2.State.PLANNED == state_;
-       assertEq(isPlanned, true);
+        bool isPlanned = EmergencyBrake.State.PLANNED == state_;
+        assertEq(isPlanned, true);
     }
 
-    function testCannotModifyUnplanned() public {
-        bytes4 burnerRole = RestrictedERC20Mock.burn.selector;
-
-        signatures.push(burnerRole);
-
-        IEmergencyBrake.Permission memory permission_ = IEmergencyBrake.Permission(address(rToken), signatures);
-
-        permissions.push(permission_);
-
-        vm.expectRevert("Emergency not planned for.");
+    function testCannotAddToUnplanned() public {
+        vm.expectRevert("Target not planned for");
         vm.prank(planner);
-        ebrake.modifyPlan(tokenAdmin, permissions);   
+        ebrake.addToPlan(tokenAdmin, permission);
+    }
+
+    function testCannotRemoveFromUnplanned() public {
+        vm.expectRevert("Target not planned for");
+        vm.prank(planner);
+        ebrake.removeFromPlan(tokenAdmin, permission);
     }
 
     function testCannotCancelUnplanned() public {
@@ -146,6 +125,18 @@ contract ZeroStateTest is ZeroState {
         vm.prank(planner);
         ebrake.terminate(tokenAdmin);
     }
+
+    function testCannotPlanRoot() public {
+        signatures.push(ROOT);
+
+        IEmergencyBrake.Permission memory permission_ = IEmergencyBrake.Permission(address(rToken), signatures);
+
+        permissions.push(permission_);
+
+        vm.expectRevert("Can't remove ROOT");
+        vm.prank(planner);
+        ebrake.plan(tokenAdmin, permissions);
+    }
 }
 
 abstract contract PlanState is ZeroState {
@@ -163,9 +154,12 @@ abstract contract PlanState is ZeroState {
 
         permissions.push(permission_);
 
-        vm.prank(planner);
+        vm.startPrank(planner);
         ebrake.plan(tokenAdmin, permissions);
-    
+        ebrake.plan(executor, permissions);
+        vm.stopPrank();
+        delete signatures;
+        delete permissions;
     }
 }
 
@@ -177,11 +171,11 @@ contract PlanStateTest is PlanState {
         emit Cancelled(tokenAdmin);
         ebrake.cancel(tokenAdmin);
         
-        (EmergencyBrakeV2.State state_,
-            ,
+        (EmergencyBrake.State state_,
+            
         ) = ebrake.plans(tokenAdmin);
 
-       bool isCancelled = EmergencyBrakeV2.State.UNPLANNED == state_;
+       bool isCancelled = EmergencyBrake.State.UNPLANNED == state_;
        assertEq(isCancelled, true);
     }
 
@@ -191,11 +185,11 @@ contract PlanStateTest is PlanState {
         vm.prank(executor);
         ebrake.execute(tokenAdmin);
 
-        (EmergencyBrakeV2.State state_,
-            ,
+        (EmergencyBrake.State state_,
+            
         ) = ebrake.plans(tokenAdmin);
         
-        bool isExecuted = EmergencyBrakeV2.State.EXECUTED == state_;
+        bool isExecuted = EmergencyBrake.State.EXECUTED == state_;
         assertEq(isExecuted, true);
 
         vm.expectRevert("Access denied");
@@ -203,19 +197,66 @@ contract PlanStateTest is PlanState {
         rToken.mint(deployer, 1e18);
     }
 
-    function testModify() public {
+    function testCannotExecuteGhostRoles() public {
+        vm.expectRevert("Permission not found");
+        vm.prank(executor);
+        ebrake.execute(executor);
+    }
+
+    function testAddToPlan() public {
+        bytes4 propose = Timelock.propose.selector;
+        bytes4 approve = Timelock.approve.selector;
+        bytes4 cancel = Timelock.cancel.selector;
+
+        signatures.push(propose);
+        signatures.push(approve);
+        signatures.push(cancel);
+
+        permission = IEmergencyBrake.Permission(address(lock), signatures);
+        
+        vm.expectEmit(true, false, false, true);
+        emit AddedTo(tokenAdmin, permission);
+        vm.prank(planner);
+        ebrake.addToPlan(tokenAdmin, permission);
+    }
+
+    function testRemoveFromPlan() public {
+        bytes4 minterRole = RestrictedERC20Mock.mint.selector;
         bytes4 burnerRole = RestrictedERC20Mock.burn.selector;
 
+        signatures.push(minterRole);
         signatures.push(burnerRole);
 
-        IEmergencyBrake.Permission memory permission_ = IEmergencyBrake.Permission(address(rToken), signatures);
-
-        permissions.push(permission_);
+        permission = IEmergencyBrake.Permission(address(rToken), signatures);
 
         vm.expectEmit(true, false, false, true);
-        emit Modified(tokenAdmin);
+        emit RemovedFrom(tokenAdmin, permission);
         vm.prank(planner);
-        ebrake.modifyPlan(tokenAdmin, permissions);
+        ebrake.removeFromPlan(tokenAdmin, permission);
+    }
+
+    function testCannotAddDuplicateSignature() public {
+        bytes4 minterRole = RestrictedERC20Mock.mint.selector;
+        bytes4 burnerRole = RestrictedERC20Mock.burn.selector;
+
+        signatures.push(minterRole);
+        signatures.push(burnerRole);
+
+        permission = IEmergencyBrake.Permission(address(rToken), signatures);
+
+        vm.expectRevert("Signature already in plan");
+        vm.prank(planner);
+        ebrake.addToPlan(tokenAdmin, permission);
+    }
+
+    function testCannotAddRoot() public {
+        signatures.push(ROOT);
+
+        permission = IEmergencyBrake.Permission(address(rToken), signatures);
+
+        vm.expectRevert("Can't remove ROOT");
+        vm.prank(planner);
+        ebrake.addToPlan(tokenAdmin, permission);
     }
 }
 
@@ -231,6 +272,28 @@ abstract contract ExecutedState is PlanState {
 contract ExecutedStateTest is ExecutedState {
      
     function testRestore() public {
+        vm.expectEmit(true, false, false, true);
+        emit Restored(tokenAdmin);
+        vm.prank(planner);
+        ebrake.restore(tokenAdmin);
 
+        (EmergencyBrake.State state_,
+            
+        ) = ebrake.plans(tokenAdmin);
+
+        bool isPlanned = EmergencyBrake.State.PLANNED == state_;
+        assertEq(isPlanned, true);
+    }
+
+    function testTerminate() public {
+        vm.expectEmit(true, false, false, true);
+        emit Terminated(tokenAdmin);
+        vm.prank(planner);
+        ebrake.terminate(tokenAdmin);
+
+        (EmergencyBrake.State state_, ) = ebrake.plans(tokenAdmin);
+
+        bool isUnplanned = EmergencyBrake.State.UNPLANNED == state_;
+        assertEq(isUnplanned, true);
     }
 }
