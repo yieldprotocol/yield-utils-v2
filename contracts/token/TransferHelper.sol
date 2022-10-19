0cd97699812bee18faf9a9ede081d2df4d7d08b1
Sabnock01 <24715302+Sabnock01@users.noreply.github.com>
2022-10-19 15:38:19 -0500

    fix pragma

--- contracts/token/TransferHelper.sol
+++ contracts/token/TransferHelper.sol
@@ -1,14 +1,16 @@
 // SPDX-License-Identifier: MIT
 // Taken from https://github.com/Uniswap/uniswap-lib/blob/master/contracts/libraries/TransferHelper.sol
-
-pragma solidity >=0.8.15;
+pragma solidity ^0.8.0;
 
 import "./IERC20.sol";
 import "../utils/RevertMsgExtractor.sol";
+import "../utils/Address.sol";
 
 
 // helper methods for interacting with ERC20 tokens and sending ETH that do not consistently return true/false
 library TransferHelper {
+    using Address for address;
+
     /// @notice Transfers tokens from msg.sender to a recipient
     /// @dev Errors with the underlying revert message if transfer fails
     /// @param token The contract address of the token which will be transferred
@@ -28,6 +30,11 @@ library TransferHelper {
         address spender, 
         uint256 value
     ) internal {
+        require(
+            (value == 0) || (token.allowance(address(this), spender) == 0),
+            "SafeERC20: approve from non-zero to non-zero allowance"
+        );
+        _callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, value));
     }
 
     /// @notice Transfers tokens from the targeted address to the given destination
@@ -53,5 +60,23 @@ library TransferHelper {
     function safeTransferETH(address payable to, uint256 value) internal {
         (bool success, bytes memory data) = to.call{value: value}(new bytes(0));
         if (!success) revert(RevertMsgExtractor.getRevertMsg(data));
+    }
+
+        /**
+     * @dev Imitates a Solidity high-level call (i.e. a regular function call to a contract), relaxing the requirement
+     * on the return value: the return value is optional (but if data is returned, it must not be false).
+     * @param token The token targeted by the call.
+     * @param data The call data (encoded using abi.encode or one of its variants).
+     */
+    function _callOptionalReturn(IERC20 token, bytes memory data) private {
+        // We need to perform a low level call here, to bypass Solidity's return data size checking mechanism, since
+        // we're implementing it ourselves. We use {Address-functionCall} to perform this call, which verifies that
+        // the target address contains contract code and also asserts for success in the low-level call.
+
+        bytes memory returndata = address(token).functionCall(data, "SafeERC20: low-level call failed");
+        if (returndata.length > 0) {
+            // Return data is optional
+            require(abi.decode(returndata, (bool)), "SafeERC20: ERC20 operation did not succeed");
+        }
     }
 }
